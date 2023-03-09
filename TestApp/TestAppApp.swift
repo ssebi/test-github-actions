f262d2a655262654803014b6d5bc74b14d207fe2
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-03-09 16:49:58 +0200

    Create iOS app

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Sebastian Vidrea on 09.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
